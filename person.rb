b107badd5bb5e76e7e07d590db69fcb6dd32dba3
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-11-15 18:15:38 -0500

    Omprove code

--- person.rb
+++ person.rb
@@ -10,20 +10,12 @@ class Person
   attr_accessor :name, :age
 
   def can_use_services?
-    if is_of_age? == true || @parent_permission == true
-      true
-    else
-      false
-    end
+    is_of_age?() || @parent_permission
   end
 
   private
 
   def is_of_age?
-    if @age >= 18
-      true
-    else
-      false
-    end
+    @age >= 18
   end
 end
