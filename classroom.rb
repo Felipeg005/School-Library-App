3525055d7c2067f14575329017479597c3aed85f
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-11-17 16:12:18 -0500

    Add classroom class with has-many and belongs-to assosiations

--- classroom.rb
+++ classroom.rb
@@ -1,7 +1,16 @@
+require 'student.rb'
+
 class Classroom
+  attr_accessor :label
+  attr_reader :students
+
   def initialize(label)
     @label = label
+    @students = []
   end
 
-  attr_accessor :label
+  def add_student(student)
+    @students.push(student)
+    student.classroom = self
+  end
 end
