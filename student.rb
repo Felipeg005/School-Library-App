8e81729ed680b09964592201a522b5e51ebcb240
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-11-19 13:14:14 -0500

    Fix student class

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require './classroom'
 
 class Student < Person
   def initialize(classroom, age, name = 'unknown', parent_permission = true)
-    super(id, age, name, parent_permission)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
