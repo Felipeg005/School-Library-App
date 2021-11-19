85e3799dab75d1442fe9005f1645f61ac77ff5ae
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-11-19 12:42:22 -0500

    Add create person teacher working as expected

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require './person'
 
 class Teacher < Person
   def initialize(specialization, age, name = 'unknown', parent_permission = true)
-    super(id, age, name, parent_permission)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
